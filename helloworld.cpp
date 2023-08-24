628d916d407858fa59a0ab5352d5d9c565afb324
MartiLezano <143094533+MartiLezano@users.noreply.github.com>
2023-08-24 08:23:56 -0500

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    //comentando ando
+    /*este si es de varias lineas*/
+    int x=20;
+    int y=32;
+    
+    cout << "el valor de x es:" <<x<< endl;
+    cout << "el valor de y es:" <<y<< endl;
+
+    string nombre="Marccelo";
+    cout <<"Hola "<< nombre <<" Este es tu semestre!!"<<endl;
+    return 0;
+
+
+}
